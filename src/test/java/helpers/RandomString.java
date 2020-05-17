2e5472580900b0a727bad7d7725f846b4df2eb63
thascalos <dolzhikds@gmail.com>
2020-05-17 12:04:49 +0300

    implements string gen for 2 task and minor code fix

--- src/test/java/helpers/RandomString.java
+++
@@ -1,8 +0,0 @@
-package helpers;
-
-import org.apache.commons.lang3.RandomStringUtils;
-
-public class RandomString {
-    String generatedString = RandomStringUtils.randomAlphanumeric(10);
-
-}
--- src/test/java/tests/InstagramTests.java
+++ src/test/java/tests/InstagramTests.java
@@ -1,13 +1,14 @@
 package tests;
 
 import io.qameta.allure.Description;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
 import static helpers.Environment.instagramLogin;
 import static helpers.Environment.instagramUrl;
 import static helpers.Environment.instagramPassword;
@@ -19,17 +20,22 @@ public class InstagramTests extends TestBase {
     @Description("Log in into account and change profile info")
     void changeInfoInInstagramProfile() {
 
+        String gen = "https://" + RandomStringUtils.randomAlphanumeric(10).toUpperCase() + ".ru/"; //generating random url
+
         open(instagramUrl);
+
         $("html").shouldHave(text("INSTAGRAM ОТ FACEBOOK"));
         $x("//input[@aria-label='Номер телефона, имя пользователя или эл. адрес']").setValue(instagramLogin);
         $x("//input[@aria-label='Пароль']").setValue(instagramPassword);
         $x("//div[text()='Войти']").waitUntil(visible, 1000).click();
-        $x("//button[text()=\"Не сейчас\"]").waitUntil(visible, 10000).click();
+        sleep(2000); //waiting for auth, yep, nasty.
+        $("html").shouldHave(text("INSTAGRAM ОТ FACEBOOK")); //wait until new page fully loaded, if not - we will logout
         open(instagramUrl + "accounts/edit/");
-        sleep(2000);
-        $("#pepWebsite").setValue("");
-        $("#pepWebsite").setValue(instagramUrl + (int)(Math.random()));
+        $("html").shouldHave(text("INSTAGRAM ОТ FACEBOOK")); //wait until new page fully loaded, if not - we will logout
+        $("#pepWebsite").setValue(gen);
         $x("//button[text()=\"Отправить\"]").waitUntil(visible, 10000).click();
+        clearBrowserCache(); //Instagram has a 'bug?' with preserving older value until page refreshes, this is workaround
+        $("#pepWebsite").shouldHave(value(gen));
     }
 
 }
