3724b2efd8ade13396767d2b108a8ae3877225b8
thascalos <dolzhikds@gmail.com>
2020-05-17 10:38:44 +0300

    New env for headless run

--- src/test/java/helpers/Environment.java
+++ src/test/java/helpers/Environment.java
@@ -2,10 +2,10 @@ package helpers;
 
 
 public class Environment {
-    public final static String
-            yandexMarketUrl = System.getProperty("yandexMarketUrl", "https://market.yandex.ru/"),
-            yandexMarketItemName = System.getProperty("yandexMarketItem", "iphone"),
-            instagramUrl = System.getProperty("instagramUrl", "https://www.instagram.com/"),
-            instagramLogin = System.getProperty("login"),
-            instagramPassword = System.getProperty("pass");
+    public final static String isHeadless = System.getProperty("isHeadless", "true");
+    public final static String yandexMarketUrl = System.getProperty("yandexMarketUrl", "https://market.yandex.ru/");
+    public final static String yandexMarketItemName = System.getProperty("yandexMarketItem", "iphone");
+    public final static String instagramUrl = System.getProperty("instagramUrl", "https://www.instagram.com/");
+    public final static String instagramLogin = System.getProperty("login");
+    public final static String instagramPassword = System.getProperty("pass");
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,12 +7,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static helpers.Environment.isHeadless;
 
 public class TestBase {
 
     @BeforeAll
     public static void setUp() {
-        //Configuration.headless = true;
+        Configuration.headless = Boolean.parseBoolean(isHeadless);
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
     }
