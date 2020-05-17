b6471f4802c986d89baedf216ce90c25a52db0d2
thascalos <dolzhikds@gmail.com>
2020-05-17 12:58:26 +0300

    Minor changes for 2 task

--- src/test/java/tests/InstagramTests.java
+++ src/test/java/tests/InstagramTests.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
 import static helpers.Environment.instagramLogin;
 import static helpers.Environment.instagramUrl;
 import static helpers.Environment.instagramPassword;
@@ -25,16 +25,17 @@ public class InstagramTests extends TestBase {
         open(instagramUrl);
 
         $("html").shouldHave(text("INSTAGRAM ОТ FACEBOOK"));
-        $x("//input[@aria-label='Номер телефона, имя пользователя или эл. адрес']").setValue(instagramLogin);
-        $x("//input[@aria-label='Пароль']").setValue(instagramPassword);
-        $x("//div[text()='Войти']").waitUntil(visible, 1000).click();
+        $("[aria-label='Номер телефона, имя пользователя или эл. адрес']").setValue(instagramLogin); //$x("//input[@aria-label='Номер телефона, имя пользователя или эл. адрес']").setValue(instagramLogin);
+        $("[aria-label='Пароль']").setValue(instagramPassword); //$x("//input[@aria-label='Пароль']").setValue(instagramPassword);
+        $(byText("Войти")).click();  //$x("//div[text()='Войти']").waitUntil(enabled, 2000).click();
+
         sleep(2000); //waiting for auth, yep, nasty.
         $("html").shouldHave(text("INSTAGRAM ОТ FACEBOOK")); //wait until new page fully loaded, if not - we will logout
+
         open(instagramUrl + "accounts/edit/");
         $("html").shouldHave(text("INSTAGRAM ОТ FACEBOOK")); //wait until new page fully loaded, if not - we will logout
         $("#pepWebsite").setValue(gen);
-        $x("//button[text()=\"Отправить\"]").waitUntil(visible, 10000).click();
-        clearBrowserCache(); //Instagram has a 'bug?' with preserving older value until page refreshes, this is workaround
+        $(byText("Отправить")).click(); //$x("//button[text()=\"Отправить\"]").waitUntil(enabled, 2000).click();
         $("#pepWebsite").shouldHave(value(gen));
     }
 
