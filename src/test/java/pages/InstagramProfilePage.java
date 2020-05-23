f330e4e38855654146989962990d1c5f47cc9a93
thascalos <dolzhikds@gmail.com>
2020-05-23 12:53:24 +0300

    Implements InstagramProfilePage and refactor InstagramProfileTests

--- src/test/java/pages/InstagramProfilePage.java
+++ src/test/java/pages/InstagramProfilePage.java
@@ -1,6 +1,53 @@
 package pages;
 
-public class InstagramProfilePage {
-//to be implemented
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class InstagramProfilePage {
+    SelenideElement loginInput = $(byName("username")),
+            passwordInput =  $(byName("password")),
+            passwordPopUp = $(byText("Не сейчас")),
+            userProfileLink = $("a[style='width: 22px; height: 22px;']"),
+            userProfileEditLink = $("[href='/accounts/edit/'] button"),
+            personalWebsite = $("#pepWebsite"),
+            saveChanges = $(byText("Отправить")),
+            htmlBody = $("html");
+
+    @Step("Авторизация по обычному логину и паролю")
+    public void authWithoutOpenID(String username, String password) {
+        loginInput.setValue(username);
+        passwordInput.setValue(password).pressEnter();
+
+        loginInput.waitUntil(disappear,10000); //waits unlil we log in, username input should dissapear
+
+        if (passwordPopUp.exists()) {
+            passwordPopUp.click(); // if remember password pop-up is presented, then click
+        }
+    }
+
+    @Step("Переход в свой профиль")
+    public void clickOnOwnUserProfile() {
+        userProfileLink.click();
+    }
+
+    @Step("Нажатие на кнопку редактирования профиля")
+    public void clickOnEditProfile() {
+        userProfileEditLink.click();
+    }
+
+    @Step("Изменение поля персонального сайта и его сохранение")
+    public void editPersonalWebsite(String site) {
+        personalWebsite.setValue(site);
+        saveChanges.click();
+        htmlBody.shouldHave(text("Профиль сохранен."));
+    }
+
+    @Step("Проверка что сохраненный сайт есть в профиле")
+    public void checkPersonalWebsite(String site) {
+        personalWebsite.shouldHave(value(site));
+    }
 }
--- src/test/java/tests/InstagramTests.java
+++ src/test/java/tests/InstagramTests.java
@@ -4,11 +4,8 @@ import io.qameta.allure.Description;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import pages.InstagramProfilePage;
 
-
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static helpers.Environment.instagramLogin;
 import static helpers.Environment.instagramUrl;
@@ -18,27 +15,19 @@ import static helpers.Environment.instagramPassword;
 public class InstagramTests extends TestBase {
 
     @Test
-    @Description("Log in into account and change website address in user profile page")
+    @Description("Changes personal website in instagram profile")
     void changeWebsiteInfoInInstagramProfile() {
 
-        String gen = "https://" + RandomStringUtils.randomAlphanumeric(10).toLowerCase() + ".ru/"; //generating random url
+        InstagramProfilePage instagramProfilePage = new InstagramProfilePage();
+        String site = "https://" + RandomStringUtils.randomAlphanumeric(10).toLowerCase() + ".ru/"; //generating random url
 
         open(instagramUrl);
-        $(byName("username")).setValue(instagramLogin);
-        $(byName("password")).setValue(instagramPassword).pressEnter();
 
-        $(byName("username")).waitUntil(disappear,10000); //waits unlil we log in, username input should dissapear
+        instagramProfilePage.authWithoutOpenID(instagramLogin, instagramPassword);
+        instagramProfilePage.clickOnOwnUserProfile();
+        instagramProfilePage.clickOnEditProfile();
+        instagramProfilePage.editPersonalWebsite(site);
 
-        if ($(byText("Не сейчас")).exists()) {
-            $(byText("Не сейчас")).click(); // if remember password pop-up is presented, then click
-        }
-
-        $("a[style='width: 22px; height: 22px;']").click();
-        $("[href='/accounts/edit/'] button").click();
-        $("#pepWebsite").setValue(gen);
-        $(byText("Отправить")).click(); //$x("//button[text()=\"Отправить\"]").waitUntil(enabled, 2000).click();
-
-        $("html").shouldHave(text("Профиль сохранен."));
-        $("#pepWebsite").shouldHave(value(gen));
+        instagramProfilePage.checkPersonalWebsite(site);
     }
 }
