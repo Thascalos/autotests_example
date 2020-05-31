3c30514a1f6ca52ce6658206ec46f951c4ef7244
thascalos <dolzhikds@gmail.com>
2020-05-31 19:07:57 +0300

    Minor fixes

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,20 +3,14 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import drivers.CustomWebDriver;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Story;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static helpers.AttachmentsHelper.*;
 
 
-@Epic("QA.GURU automation course")
-@Story("Google tests with video")
-@Tag("google")
 class TestBase {
 
     @BeforeEach
